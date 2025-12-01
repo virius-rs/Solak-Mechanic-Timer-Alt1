dcc09b646ac1fcb2f950ccf3d4613224d279e47e
Virius <jamesvirius@gmail.com>
2025-12-01 01:23:38 +0000

    Removed ErrorBoundary import

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { ErrorBoundary } from "./components";
-import "./index.css";
+import "./index.css"; 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <App />
   </React.StrictMode>
 );
