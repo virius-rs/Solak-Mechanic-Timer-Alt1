4ec0dfe371841151a2bfe3282367f212d3f66b7d
Virius <jamesvirius@gmail.com>
2025-12-01 01:29:19 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// 1. Import ChatReader (Named export uses curly braces {})
+// 1. Import ChatReader directly (Named export)
 import { ChatReader } from "./ChatReader";
 import "./index.css"; 
 
+// 2. Render simple app without ErrorBoundary or StrictMode
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  // 2. Wrap ChatReader in the ErrorBoundary
-  <ErrorBoundary>
-    <ChatReader />
-  </ErrorBoundary>
+  <ChatReader />
 );
-
