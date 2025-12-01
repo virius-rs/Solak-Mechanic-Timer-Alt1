9926b8b83b0851c74994ea65ba45ddae6dfbe3ad
Virius <jamesvirius@gmail.com>
2025-12-01 01:26:07 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+// 1. Import ChatReader (Named export uses curly braces {})
+import { ChatReader } from "./ChatReader";
+import { ErrorBoundary } from "./components"; 
 import "./index.css"; 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // 2. Wrap ChatReader in the ErrorBoundary
+  <ErrorBoundary>
+    <ChatReader />
+  </ErrorBoundary>
 );
