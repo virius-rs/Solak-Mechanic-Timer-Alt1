909d7f838e87eec53beed15b8860269c27e73dda
Virius <jamesvirius@gmail.com>
2025-12-01 01:27:09 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // 1. Import ChatReader (Named export uses curly braces {})
 import { ChatReader } from "./ChatReader";
-import { ErrorBoundary } from "./components"; 
 import "./index.css"; 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
@@ -11,3 +10,4 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <ChatReader />
   </ErrorBoundary>
 );
+
