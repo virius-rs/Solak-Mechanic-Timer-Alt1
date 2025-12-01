85cc5ba9ca8041929680809fa1bf0439a14ea3a7
Virius <jamesvirius@gmail.com>
2025-12-01 01:34:30 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// 1. Import ChatReader directly (Named export)
-import { ChatReader } from "./ChatReader";
-import "./index.css"; 
+import App from "./App";
+import { ErrorBoundary } from "./components";
+import "./index.css";
 
-// 2. Render simple app without ErrorBoundary or StrictMode
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <ChatReader />
+  <React.StrictMode>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>
 );
